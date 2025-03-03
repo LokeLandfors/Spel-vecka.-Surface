b67cdbc30bea8efa963d19aab00434a1a3356603
cersait <atillaesen162@gmail.com>
2025-03-03 14:24:57 +0100

    Teleporter key is E

--- Surface/Assets/scripts/Teleporter.cs
+++ Surface/Assets/scripts/Teleporter.cs
@@ -1,18 +1,44 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject portal2;
+
+    private bool isPlayerInRange = false; // Track if the player is near the teleporter
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            TeleportPlayer();
+        }
+    }
+
+    private void TeleportPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            player.transform.position = portal2.transform.position;
+        }
     }
 }
