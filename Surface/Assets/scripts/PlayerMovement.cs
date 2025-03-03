1074260ecf6ea4725ba34e9bdd11720598d4bc52
LokeLandfors <loke.landfors@elev.ga.lbs.se>
2025-03-03 12:50:37 +0100

    5
    scenes. scripts. map

---
+++ Surface/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
