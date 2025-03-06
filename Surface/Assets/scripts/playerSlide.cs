881e498e156f6cf944226e03548d89cb0fbb4137
WaykeRubi <wayk.rubilarvangelius@elev.ga.lbs.se>
2025-03-06 14:54:04 +0100

    Added sliding and fixed player movement for the 50th time

---
+++ Surface/Assets/scripts/playerSlide.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSlide : MonoBehaviour
+{
+
+    public bool isSliding = false;
+    public PlayerMovement PL;
+    public Rigidbody2D rgb;
+    public Animator anim;
+    public BoxCollider2D regularCall;
+    public BoxCollider2D slideCall;
+
+    public float slidespeed = 1800f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+            PerfromSlide();
+    }
+
+
+    private void PerfromSlide()
+    {
+        isSliding = true;
+        anim.SetBool("IsSlide", true);
+
+        regularCall.enabled = false;
+        slideCall.enabled = true;
+
+        if (!PL.sprite.flipX)
+        {
+            rgb.AddForce(Vector2.right * slidespeed);
+        }
+        else
+        {
+            rgb.AddForce(Vector2.left * slidespeed);
+        }
+        StartCoroutine("stopSlide");
+    }
+
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds(0.8f);
+        anim.Play("PlayerIdle");
+        anim.SetBool("IsSlide", false);
+        regularCall.enabled = true;
+        slideCall.enabled = false;
+        isSliding = false;
+
+    }
+
+}
